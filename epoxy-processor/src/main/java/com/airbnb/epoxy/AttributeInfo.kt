da7fa45e743ab2df4905c9cf1dd47100f3841b5b
Suhaib Mujahid <suhaibmujahid@gmail.com>
2020-08-15 01:34:43 -0400

    Update AttributeInfo.kt

--- epoxy-processor/src/main/java/com/airbnb/epoxy/AttributeInfo.kt
+++ epoxy-processor/src/main/java/com/airbnb/epoxy/AttributeInfo.kt
@@ -3,6 +3,8 @@ package com.airbnb.epoxy
 
 
 
+
+
 import com.airbnb.epoxy.GeneratedModelInfo.AttributeGroup
 import com.airbnb.epoxy.Utils.EPOXY_MODEL_TYPE
 import com.airbnb.epoxy.Utils.isSubtypeOfType
