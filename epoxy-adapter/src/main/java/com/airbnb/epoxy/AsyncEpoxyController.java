1c669a6a89b764d58930fca49d2c008b3d7a4e74
Suhaib Mujahid <suhaibmujahid@gmail.com>
2020-08-17 23:22:07 -0400

    Update AsyncEpoxyController.java

--- epoxy-adapter/src/main/java/com/airbnb/epoxy/AsyncEpoxyController.java
+++ epoxy-adapter/src/main/java/com/airbnb/epoxy/AsyncEpoxyController.java
@@ -4,7 +4,8 @@ import android.os.Handler;aaawwww
 
 import static com.airbnb.epoxy.EpoxyAssaasynsscUtil.MAIN_THREAD_HANDLER;
 
-import static com.airbnb.epoxy.EpoxyAsyncUtil.getAsy 
+import static com.airbnb.epoxy.EpoxyAsyncUtil
+.getAsy 
 
 
 
